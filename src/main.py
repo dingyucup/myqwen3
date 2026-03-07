19c0f5dfaa60be9b2b29c24cb853d2bbd78a2c8b
dingyucup <dingyucup@163.com>
2026-03-07 21:10:07 +0800

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,39 +1,56 @@
-from argparse import ArgumentParser
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import torch
+from src.predict.predictor_steam import StreamPredictor
+from src.model.qwen3_model import Qwen3Model
+from src.configuration.config import *
+from src.preprocess.tokenizer import Tokenizer
+from src.transformer.attention_kv_cache import add_kv_cache_to_model
+
+def ai_assistant(
+        model: Qwen3Model,
+        tokenizer: Tokenizer,
+        device: torch.device
+):
+    model = add_kv_cache_to_model(model)
+    print("你好！我是知识问答助手，请你提问，如需退出请输入 exit。")
+    while True:
+        user_input = input("问题 >>>")
+        question = user_input.strip().replace("?", "")
+        if question == "exit":
+            break
+
+        print("回答 >>>", end="")
+        StreamPredictor.generate_answer_stream(
+            question_text=question,
+            model=model,
+            tokenizer=tokenizer,
+            max_num_new_tokens=50,
+            context_size=CONTEXT_LENGTH,
+            # temperature=0.3,
+            top_p=0.95,
+            eos_id=tokenizer.eos_id,
+            device=device,
+            repetition_penalty=1.2
+        )
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # torch.manual_seed(123)
+    # lora_model = Qwen3Model.from_pretrained(LORA_MODELS_DIR/MERGED_LORA_MODEL_NAME)
+    lora_model = Qwen3Model.from_pretrained(MODELS_DIR/"dpo_best_model_sft.pt")
+    # lora_model = Qwen3Model.from_lora_pretrained(MODELS_DIR/"dpo_best_model_with_lora_beta_0_0_5.pt")
+    lora_model.to(device)
+
+    tokenizer_t = Tokenizer.from_vocab(special_char_set=SPECIAL_CHAR_SET)
+
+    ai_assistant(model=lora_model, tokenizer=tokenizer_t, device=device)
+
 
 if __name__ == "__main__":
-    parser = ArgumentParser(usage="usage: main.py action")
+    main()
 
-    parser.add_argument(
-        "action",
-    choices=["train", "predict", "evaluate", "serve", "preprocess", "help"]
-    )
 
-    args = parser.parse_args()
-    action = args.action
 
-    match action:
-        case "preprocess":
-            from preprocess.tokenizer import Tokenizer
-            from preprocess.text_processor import Preprocessor
-            tokenizer = Tokenizer.from_tiktoken("gpt2")
-            preprocessor = Preprocessor(tokenizer)
-            preprocessor.processing()
-        case "train":
-            pass
-        case "predict":
-            pass
-        case "help":
-            print("pyton main.py [action]")
-            print("="*5, "action", "="*5)
-            print("preprocess: 数据预处理")
-            print("train: 训练")
-            print("evaluate: 评价")
-            print("serve: 服务")
-            print("predict: 预测")
-            print("help: 帮助")
-        case "evaluate":
-            pass
-        case "serve":
-            pass
-        case _:
-            print("帮助命令: python main.py help")
